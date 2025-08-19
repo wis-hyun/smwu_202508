13520aa0f82d7a361d4998ffe568d3a520ac44e6
wis-hyun <khyun8576@naver.com>
2025-08-19 17:06:11 +0900

    column_practice_screen 추가

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -3,6 +3,8 @@ import 'package:smwu_202508/screen/column/column_practice_screen.dart';
 import 'package:smwu_202508/screen/column/column_screen.dart';
 import 'package:smwu_202508/screen/container/container_practice_screen.dart';
 import 'package:smwu_202508/screen/container/container_screen.dart';
+import 'package:smwu_202508/screen/row/row_practice_screen.dart';
+import 'package:smwu_202508/screen/row/row_screen.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
@@ -74,7 +76,32 @@ class ButtonScreen extends StatelessWidget {
               child: Text("ColumnPracticeScreen"),
             ),
 
-
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RowScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("Row"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RowPracticeScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("RowPracticeScreen"),
+            ),
           ],
         ),
       ),
