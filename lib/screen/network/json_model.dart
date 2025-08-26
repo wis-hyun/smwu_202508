c4428594576b545708204f83d66d018539f9c2f2
wis-hyun <khyun8576@naver.com>
2025-08-26 15:19:14 +0900

    json_model 추가

---
+++ lib/screen/network/json_model.dart
@@ -0,0 +1,65 @@
+class JsonModel {
+  String id;
+  int number;
+  Info? info;
+
+  JsonModel(this.id, this.number, this.info);
+  factory JsonModel.fromJson(Map<String, dynamic> json){
+    return JsonModel(
+        json['id'] ?? '',
+        json['number'] ?? 0,
+        json['info'] == null ? null : Info.fromJson(json['info']));
+  }
+
+
+  @override
+  String toString() {
+    return 'JsonModel{id: $id, number: $number, info: $info}';
+  }
+
+  //int age;
+  //double weight;
+  //string name;
+}
+
+class Info {
+  int age;
+  double weight;
+  String name;
+  Description? description; //null을 허용하는 타입
+
+  Info(this.age, this.weight, this.name, this.description);
+  factory Info.fromJson(Map<String, dynamic> json){
+    return Info(
+        json['age'] ?? 0,
+        json['weight'] ?? 0.0,
+        json['name'] ?? '',
+        json['description'] == null ? null : Description.fromJson(json['description']));
+  }
+
+  @override
+  String toString() {
+    return 'Info{age: $age, weight: $weight, name: $name, description: $description}';
+  }
+}
+
+class Description {
+  String career;
+  String when;
+
+  Description(this.career, this.when);
+
+  //factory : named 생성자와 유사함
+  //json에는 career, when 이 들어옴
+  factory Description.fromJson(Map<String, dynamic> json) {
+    return Description(
+        json['career'] ?? '', //null 일때 공백으로 넣어줌.
+        json['when'] ?? '',
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Description{career: $career, when: $when}';
+  }
+}
--- lib/screen/network/member_list_screen.dart
+++ lib/screen/network/member_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:smwu_202508/screen/network/json_model.dart';
 
 class MemberListScreen extends StatefulWidget {
   const MemberListScreen({super.key});
@@ -9,19 +10,40 @@ class MemberListScreen extends StatefulWidget {
 }
 
 class _MemberListScreenState extends State<MemberListScreen> {
-
-  Dio dio = Dio(BaseOptions(baseUrl: 'https://9d8cdaee67f2.ngrok-free.app'));
+  Dio dio = Dio(BaseOptions(baseUrl: 'https://d0a701c36a9e.ngrok-free.app'));
+  Dio dio2 = Dio(
+    BaseOptions(
+      baseUrl:
+          'https://online-lecture-data.s3.ap-northeast-2.amazonaws.com/data.json',
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('member look')),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              dio.get('/api/v1/member/all');
+            },
+            child: Text('get data'),
+          ),
 
-    return Scaffold(appBar: AppBar(title: Text('member look'),),
-    body: Column(
-      children: [
-        ElevatedButton(onPressed: (){
-          dio.get('/api/v1/member/all');
-        }, child: Text('get data')),
-      ],
-    ),);
+          ElevatedButton(
+            onPressed: () {
+              dio2.get('').then((value) {
+                print(value);
+                var jsonModel = JsonModel.fromJson(value.data);
+                print(jsonModel);
+
+              });
+            },
+            child: Text('get json'),
+          ),
+        ],
+      ),
+    );
   }
 }
