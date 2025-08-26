a3feb25f532db389dc33bcb2b22e52a42a92d106
wis-hyun <khyun8576@naver.com>
2025-08-26 16:38:32 +0900

    member list 추가(ft.멘토님) - 이미 만들어진것에다가 기능 추가

--- lib/screen/network/member_list_screen.dart
+++ lib/screen/network/member_list_screen.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:smwu_202508/screen/network/json_model.dart';
 
+import 'member.dart';
+
 class MemberListScreen extends StatefulWidget {
   const MemberListScreen({super.key});
 
@@ -9,7 +11,9 @@ class MemberListScreen extends StatefulWidget {
   State<MemberListScreen> createState() => _MemberListScreenState();
 }
 
+
 class _MemberListScreenState extends State<MemberListScreen> {
+
   Dio dio = Dio(BaseOptions(baseUrl: 'https://d0a701c36a9e.ngrok-free.app'));
   Dio dio2 = Dio(
     BaseOptions(
@@ -17,6 +21,7 @@ class _MemberListScreenState extends State<MemberListScreen> {
           'https://online-lecture-data.s3.ap-northeast-2.amazonaws.com/data.json',
     ),
   );
+  List<Member> memberList = [ ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +31,29 @@ class _MemberListScreenState extends State<MemberListScreen> {
         children: [
           ElevatedButton(
             onPressed: () {
-              dio.get('/api/v1/member/all');
+              dio.get('/api/v1/member/all').then((value){
+                if(value.data is Iterable){}
+                memberList = (value.data as Iterable).map((e) => Member.fromJson(e)).toList();
+                print('$memberList');
+                setState(() {
+
+                });
+              });
             },
             child: Text('get data'),
+          ),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: memberList.length,
+              itemBuilder: (context, index) {
+                final member = memberList[index];
+                return ListTile(
+                  title: Text("메일 : ${member.email}"),
+                  subtitle: Text("설명 : ${member.description}"),
+                );
+              },
+            ),
           ),
 
         ],
